aa91dd55f1bc5cb458a8c97fcbc562dd264518e6
ClaytonDewey <clay@dryadmedia.com>
2024-03-07 18:27:18 -0600

    Display individual items in cart

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
+export { default as CartTile } from './CartTile';
 export { default as Header } from './Header';
 export { default as ProductTile } from './ProductTile';
--- src/store/slices/cart-slice.js
+++ src/store/slices/cart-slice.js
@@ -11,8 +11,8 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-      console.log(action);
-      state.push(action.payload);
+      // state.push(action.payload);
+      return [...state, action.payload];
     },
     removeFromCart(state, action) {
       return state.filter((item) => item.id !== action.payload);
